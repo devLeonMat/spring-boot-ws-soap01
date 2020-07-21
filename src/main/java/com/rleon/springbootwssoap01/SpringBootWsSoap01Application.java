24700e3be184954208093220737e12f07c27f1e5
leon matias <roslin.leon.cix@outlook.com>
2020-07-21 09:32:25 -0500

    add change

---
+++ src/main/java/com/rleon/springbootwssoap01/SpringBootWsSoap01Application.java
@@ -0,0 +1,13 @@
+package com.rleon.springbootwssoap01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWsSoap01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWsSoap01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rleon/springbootwssoap01/SpringBootWsSoap01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rleon.springbootwssoap01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWsSoap01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
